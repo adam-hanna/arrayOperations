3256c043d0cc113913f4863c58d86663e3f53e18
Adam Hanna <adam@faze-1.com>
2015-03-26 13:51:04 -0700

    fixed broken difference algorithm. Made improvements to intersection algorithm

--- arrayOperations.go
+++ arrayOperations.go
@@ -2,38 +2,24 @@
 // e.x. a1 = [1 2 2 4 6]; a2 = [2 4 5]
 // Intersect(a1, a2) >> [2 4]
 func IntersectString(args ...[]string) []string {
-	// hold the values of the arrays;
-	// the first map of uint64 represents the index of args
-	// the second map of strings represents that values in the arg arrays
-	tempMap := make(map[uint64]map[string]uint8)
-	for n, arg := range args {
-		tempMap[uint64(n)] = make(map[string]uint8)
+	// create a map to count all the instances of the strings
+	tempMap := make(map[string]int)
+	for _, arg := range args {
 		for idx := range arg {
-			tempMap[uint64(n)][arg[idx]] = 0
-		}
-	}
-
-	// hold the interesctions; incremental keys are successive intersections
-	intersectMap := make(map[uint64]map[string]uint8)
-	intersectMap[0] = make(map[string]uint8)
-	// we need to write the first one
-	for key := range tempMap[0] {
-		intersectMap[0][key] = 0
-	}
-
-	for i := 1; i < len(tempMap); i++ {
-		intersectMap[uint64(i)] = make(map[string]uint8)
-		for key := range tempMap[uint64(i)] {
-			if _, ok := intersectMap[uint64(i-1)][key]; ok {
-				intersectMap[uint64(i)][key] = 0
+			if _, ok := tempMap[arg[idx]]; ok {
+				tempMap[arg[idx]]++
+			} else {
+				tempMap[arg[idx]] = 1
 			}
 		}
 	}
 
-	// write the final val of the intersectMap to an array and return
+	// find the keys equal to the length of the input args
 	tempArray := make([]string, 0)
-	for key := range intersectMap[uint64(len(intersectMap)-1)] {
-		tempArray = append(tempArray, key)
+	for key := range tempMap {
+		if tempMap[key] == len(args) {
+			tempArray = append(tempArray, key)
+		}
 	}
 
 	return tempArray
@@ -62,43 +48,24 @@ func UnionString(args ...[]string) []string {
 // e.x. a1 = [1 2 2 4 6]; a2 = [2 4 5]
 // Difference(a1, a2) >> [5 6]
 func DifferenceString(args ...[]string) []string {
-	// hold the values of the arrays;
-	// the first map of uint64 represents the index of args
-	// the second map of strings represents that values in the arg arrays
-	tempMap := make(map[uint64]map[string]uint8)
-	for n, arg := range args {
-		tempMap[uint64(n)] = make(map[string]uint8)
+	// craete a map to count all the instances of the strings
+	tempMap := make(map[string]int)
+	for _, arg := range args {
 		for idx := range arg {
-			tempMap[uint64(n)][arg[idx]] = 0
-		}
-	}
-
-	// hold the differences
-	diffMap := make(map[uint64]map[string]uint8)
-	diffMap[0] = make(map[string]uint8)
-	// we need to start with one of the arrays
-	for key := range tempMap[uint64(0)] {
-		diffMap[0][key] = 0
-	}
-
-	for i := 1; i < len(tempMap); i++ {
-		diffMap[uint64(i)] = make(map[string]uint8)
-		for key := range diffMap[uint64(i-1)] {
-			if _, ok := tempMap[uint64(i)][key]; !ok {
-				diffMap[uint64(i)][key] = 0
-			}
-		}
-		for key := range tempMap[uint64(i)] {
-			if _, ok := diffMap[uint64(i-1)][key]; !ok {
-				diffMap[uint64(i)][key] = 0
+			if _, ok := tempMap[arg[idx]]; ok {
+				tempMap[arg[idx]]++
+			} else {
+				tempMap[arg[idx]] = 1
 			}
 		}
 	}
 
-	// write the final val of the diffMap to an array and return
+	// find the keys with only 1 instance
 	tempArray := make([]string, 0)
-	for key := range diffMap[uint64(len(diffMap)-1)] {
-		tempArray = append(tempArray, key)
+	for key := range tempMap {
+		if tempMap[key] == 1 {
+			tempArray = append(tempArray, key)
+		}
 	}
 
 	return tempArray
