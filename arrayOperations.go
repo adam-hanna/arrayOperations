41067331a8f0da14cdc909618c36bb62e56e27b6
Adam Hanna <adam@faze-1.com>
2015-03-29 14:05:38 -0700

    added package name

--- arrayOperations.go
+++ arrayOperations.go
@@ -1,3 +1,5 @@
+package arrayOperations
+
 /* ***************************************************************
 *
 * THIS SECTION IS FOR STRINGS
--- benchmark.go
+++ benchmark.go
@@ -1,3 +1,4 @@
+/*
 package main
 
 import (
@@ -31,3 +32,4 @@ func main() {
 	}
 
 }
+*/
