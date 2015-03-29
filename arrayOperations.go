e79246eda621aa196f360d1a6466f6eebbd0f217
Adam Hanna <adam@faze-1.com>
2015-03-29 14:19:08 -0700

    moved benchmark to a private function

--- arrayOperations.go
+++ arrayOperations.go
@@ -1,5 +1,13 @@
 package arrayOperations
 
+import (
+// "fmt"
+// "math"
+// "math/rand"
+// "strconv"
+// "time"
+)
+
 /* ***************************************************************
 *
 * THIS SECTION IS FOR STRINGS
@@ -372,4 +380,37 @@ func DistinctUint64(arg []uint64) []uint64 {
 		tempArray = append(tempArray, key)
 	}
 	return tempArray
+}
+
+/* ***************************************************************
+*
+* THIS SECTION IS FOR a little helper benchmark script which can be
+* used to test the relative changes in performance when making
+* changes to these algorithms.
+*
+* To use, un-comment out the packages in import.
+*
+/* *************************************************************** */
+
+func benchmark() {
+	a1 := make([]string, 0)
+	a2 := make([]string, 0)
+
+	i := 1
+	j := 1
+
+	for i = 1; i <= 7; i++ {
+		// create the arrays
+		arrLength := len(a1)
+		for j = 0; j < ((int(math.Pow(10, float64(i))) / 2) - arrLength); j++ {
+			a1 = append(a1, strconv.Itoa(rand.Int()))
+			a2 = append(a2, strconv.Itoa(rand.Int()))
+		}
+
+		t0 := time.Now()
+		IntersectString(a1, a2)
+		t1 := time.Now()
+		fmt.Printf("n of %v took %v to run.\n", 2*len(a1), t1.Sub(t0))
+	}
+
 }
