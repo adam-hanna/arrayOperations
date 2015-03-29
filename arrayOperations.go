87283862a7a7054a6a9611f6d4ed090c3260a388
Adam Hanna <adam@faze-1.com>
2015-03-29 12:33:39 -0700

    added support for multiDimensional array input

--- arrayOperations.go
+++ arrayOperations.go
@@ -34,6 +34,36 @@ func IntersectString(args ...[]string) []string {
 	return tempArray
 }
 
+// find the intersection of two arrays.
+// e.g. a1 = [1 2 2 4 6]; a2 = [2 4 5]
+// Intersect(a1, a2) >> [2 4]
+func IntersectStringArr(arr [][]string) []string {
+	// create a map to count all the instances of the strings
+	arrLength := len(arr)
+	tempMap := make(map[string]int)
+	for idx1 := range arr {
+		tempArr := DistinctString(arr[idx1])
+		for idx2 := range tempArr {
+			// how many times have we encountered this elem?
+			if _, ok := tempMap[tempArr[idx2]]; ok {
+				tempMap[tempArr[idx2]]++
+			} else {
+				tempMap[tempArr[idx2]] = 1
+			}
+		}
+	}
+
+	// find the keys equal to the length of the input args
+	tempArray := make([]string, 0)
+	for key, val := range tempMap {
+		if val == arrLength {
+			tempArray = append(tempArray, key)
+		}
+	}
+
+	return tempArray
+}
+
 // find the union of two arrays.
 // e.g. a1 = [1 2 2 4 6]; a2 = [2 4 5]
 // Union(a1, a2) >> [1 2 4 5 6]
@@ -45,6 +75,29 @@ func UnionString(args ...[]string) []string {
 	for _, arg := range args {
 		for idx := range arg {
 			tempMap[arg[idx]] = 0
+		}
+	}
+
+	// the map keys are now unique instances of all of the array contents
+	tempArray := make([]string, 0)
+	for key := range tempMap {
+		tempArray = append(tempArray, key)
+	}
+
+	return tempArray
+}
+
+// find the union of two arrays.
+// e.g. a1 = [1 2 2 4 6]; a2 = [2 4 5]
+// Union([a1, a2]) >> [1 2 4 5 6]
+func UnionStringArr(arr [][]string) []string {
+	// create a temporary map to hold the contents of the arrays
+	tempMap := make(map[string]uint8)
+
+	// write the contents of the arrays as keys to the map. The map values don't matter
+	for idx1 := range arr {
+		for idx2 := range arr[idx1] {
+			tempMap[arr[idx1][idx2]] = 0
 		}
 	}
 
@@ -71,6 +124,35 @@ func DifferenceString(args ...[]string) []string {
 				tempMap[tempArr[idx]]++
 			} else {
 				tempMap[tempArr[idx]] = 1
+			}
+		}
+	}
+
+	// write the final val of the diffMap to an array and return
+	tempArray := make([]string, 0)
+	for key, val := range tempMap {
+		if val == 1 {
+			tempArray = append(tempArray, key)
+		}
+	}
+
+	return tempArray
+}
+
+// find the difference of two arrays.
+// e.g. a1 = [1 2 2 4 6]; a2 = [2 4 5]
+// Difference([a1, a2]) >> [5 6]
+func DifferenceStringArr(arr [][]string) []string {
+	// create a temporary map to hold the contents of the arrays
+	tempMap := make(map[string]int)
+	for idx1 := range arr {
+		tempArr := DistinctString(arr[idx1])
+		for idx2 := range tempArr {
+			// how many times have we encountered this elem?
+			if _, ok := tempMap[tempArr[idx2]]; ok {
+				tempMap[tempArr[idx2]]++
+			} else {
+				tempMap[tempArr[idx2]] = 1
 			}
 		}
 	}
@@ -141,7 +223,7 @@ func IntersectUint64(args ...[]uint64) []uint64 {
 
 // find the intersection of two arrays.
 // e.g. a1 = [1 2 2 4 6]; a2 = [2 4 5]
-// Intersect(a1, a2) >> [2 4]
+// Intersect([a1, a2]) >> [2 4]
 func IntersectUint64Arr(arr [][]uint64) []uint64 {
 	// create a map to count all the instances of the strings
 	arrLength := len(arr)
@@ -192,6 +274,29 @@ func UnionUint64(args ...[]uint64) []uint64 {
 	return tempArray
 }
 
+// find the union of two arrays.
+// e.g. a1 = [1 2 2 4 6]; a2 = [2 4 5]
+// Union([a1, a2]) >> [1 2 4 5 6]
+func UnionUint64Arr(arr [][]uint64) []uint64 {
+	// create a temporary map to hold the contents of the arrays
+	tempMap := make(map[uint64]uint8)
+
+	// write the contents of the arrays as keys to the map. The map values don't matter
+	for idx1 := range arr {
+		for idx2 := range arr[idx1] {
+			tempMap[arr[idx1][idx2]] = 0
+		}
+	}
+
+	// the map keys are now unique instances of all of the array contents
+	tempArray := make([]uint64, 0)
+	for key := range tempMap {
+		tempArray = append(tempArray, key)
+	}
+
+	return tempArray
+}
+
 // find the difference of two arrays.
 // e.g. a1 = [1 2 2 4 6]; a2 = [2 4 5]
 // Difference(a1, a2) >> [5 6]
@@ -206,6 +311,35 @@ func DifferenceUint64(args ...[]uint64) []uint64 {
 				tempMap[tempArr[idx]]++
 			} else {
 				tempMap[tempArr[idx]] = 1
+			}
+		}
+	}
+
+	// write the final val of the diffMap to an array and return
+	tempArray := make([]uint64, 0)
+	for key, val := range tempMap {
+		if val == 1 {
+			tempArray = append(tempArray, key)
+		}
+	}
+
+	return tempArray
+}
+
+// find the difference of two arrays.
+// e.g. a1 = [1 2 2 4 6]; a2 = [2 4 5]
+// Difference([a1, a2]) >> [5 6]
+func DifferenceUint64Arr(arr [][]uint64) []uint64 {
+	// create a temporary map to hold the contents of the arrays
+	tempMap := make(map[uint64]int)
+	for idx1 := range arr {
+		tempArr := DistinctUint64(arr[idx1])
+		for idx2 := range tempArr {
+			// how many times have we encountered this elem?
+			if _, ok := tempMap[tempArr[idx2]]; ok {
+				tempMap[tempArr[idx2]]++
+			} else {
+				tempMap[tempArr[idx2]] = 1
 			}
 		}
 	}
